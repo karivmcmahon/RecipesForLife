540024ab55068fa31d7e1f1d648b21dd314b34a7
karivmcmahon <kmc_xo@hotmail.co.uk>
2014-12-27 20:56:09 +0000

    More dialog stuff

--- RecipesForLife/gen/com/example/recipesforlife/R.java
+++ RecipesForLife/gen/com/example/recipesforlife/R.java
@@ -30,35 +30,49 @@ public final class R {
         public static final int shape=0x7f020003;
     }
     public static final class id {
-        public static final int action_settings=0x7f090018;
+        public static final int action_settings=0x7f090024;
+        public static final int additionalView=0x7f090018;
         public static final int bioEditText=0x7f090006;
         public static final int bioTextView=0x7f090005;
-        public static final int button1=0x7f090016;
+        public static final int bioView=0x7f09001c;
+        public static final int button1=0x7f090022;
+        public static final int cityEdit=0x7f09001d;
         public static final int cityEditText=0x7f090008;
         public static final int cityTextView=0x7f090007;
+        public static final int cityView=0x7f090019;
+        public static final int countryEdit=0x7f09001b;
         public static final int countryEditText=0x7f090004;
         public static final int countryText=0x7f090003;
-        public static final int editText1=0x7f090015;
-        public static final int editText2=0x7f090014;
+        public static final int countryView=0x7f09001a;
+        public static final int createView=0x7f090011;
+        public static final int editText1=0x7f090017;
+        public static final int editText2=0x7f090016;
         public static final int emailEditText=0x7f09000e;
         public static final int emailView=0x7f09000d;
-        public static final int imageView1=0x7f090011;
+        public static final int imageView1=0x7f090020;
         public static final int interestEditText=0x7f090009;
         public static final int interestTextView=0x7f09000a;
+        public static final int interestView=0x7f09001e;
+        public static final int nameEdit=0x7f090014;
         public static final int nameEditText1=0x7f09000c;
+        public static final int nameView=0x7f090012;
         public static final int nameView1=0x7f09000b;
+        public static final int nextButton=0x7f090015;
         public static final int passwordEditText=0x7f090010;
         public static final int passwordTextView=0x7f09000f;
         public static final int passwordView=0x7f090013;
         public static final int secondNameEditText=0x7f090001;
         public static final int secondNameView=0x7f090002;
+        public static final int signUpButton=0x7f09001f;
         public static final int signUpButton1=0x7f090000;
-        public static final int signUpView=0x7f090017;
-        public static final int textView1=0x7f090012;
+        public static final int signUpView=0x7f090023;
+        public static final int textView1=0x7f090021;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int signupsigninactivity=0x7f030001;
+        public static final int signupcustomdialog=0x7f030001;
+        public static final int signupnextcustomdialog=0x7f030002;
+        public static final int signupsigninactivity=0x7f030003;
     }
     public static final class menu {
         public static final int main=0x7f080000;
--- RecipesForLife/src/com/example/recipesforlife/views/SignUpSignInActivity.java
+++ RecipesForLife/src/com/example/recipesforlife/views/SignUpSignInActivity.java
@@ -6,8 +6,14 @@ import com.example.recipesforlife.R;
 import com.example.recipesforlife.models.databaseConnection;
 
 import android.app.Activity;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnDismissListener;
 import android.graphics.Color;
 import android.graphics.Typeface;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.text.Html;
 import android.util.Log;
@@ -16,16 +22,18 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.View.OnTouchListener;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class SignUpSignInActivity extends Activity {
 	
+	Typeface typeFace;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.signupsigninactivity);
-		Typeface typeFace=Typeface.createFromAsset(getAssets(),"fonts/elsie.ttf");
+		typeFace=Typeface.createFromAsset(getAssets(),"fonts/elsie.ttf");
 		TextView titleView = (TextView) findViewById(R.id.textView1);
 		titleView.setTypeface(typeFace);
 		titleView.setTextSize(24);
@@ -44,7 +52,7 @@ public class SignUpSignInActivity extends Activity {
 		button.setTextColor(Color.parseColor("#FFFFFFFF"));
 		
 		TextView  signupView = (TextView) findViewById(R.id.signUpView);
-		signupView.setText(Html.fromHtml("<p><u>Click here to sign up</u></p>"));
+		signupView.setText(Html.fromHtml("<p><u>Create an account</u></p>"));
 		signupView.setTypeface(typeFace);
 		signupView.setTextColor(Color.parseColor("#FFFFFFFF"));
 		signupView.setOnTouchListener(new OnTouchListener() {
@@ -53,9 +61,102 @@ public class SignUpSignInActivity extends Activity {
 
 			@Override
 			public boolean onTouch(View v, MotionEvent event) {
-				// TODO Auto-generated method stub
-				Log.v("CLICK", "CLICK");
-				return false;
+				if(event.getAction()  == MotionEvent.ACTION_DOWN)
+				{
+				// TODO Auto-generated method stubS
+				final Dialog dialog = new Dialog(SignUpSignInActivity.this);
+				dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+				Drawable d = new ColorDrawable(Color.parseColor("#FFFFFFFF"));
+				d.setAlpha(80);
+
+
+				//dialog.getWindow().setBackgroundDrawableResource(R.drawable.shape);
+				dialog.setContentView(R.layout.signupcustomdialog);
+				dialog.getWindow().setBackgroundDrawable(d);
+		
+	 
+				TextView nameV = (TextView)	 dialog.findViewById(R.id.nameView);
+				nameV.setTypeface(typeFace);
+				nameV.setTextColor(Color.parseColor("#FFFFFFFF"));
+				
+				TextView emailV = (TextView) dialog.findViewById(R.id.emailView);
+				emailV.setTypeface(typeFace);
+				emailV.setTextColor(Color.parseColor("#FFFFFFFF"));
+				
+				TextView passwordV = (TextView)	 dialog.findViewById(R.id.passwordView);
+				passwordV.setTypeface(typeFace);
+				passwordV.setTextColor(Color.parseColor("#FFFFFFFF"));
+				
+				TextView createV = (TextView)	 dialog.findViewById(R.id.createView);
+				createV.setTypeface(typeFace);
+				createV.setTextSize(20);
+				createV.setTextColor(Color.parseColor("#FFFFFFFF"));
+				
+				dialog.show(); 
+				Button dialogButton = (Button) dialog.findViewById(R.id.nextButton);
+				dialogButton.setTypeface(typeFace);
+				dialogButton.setTextColor(Color.parseColor("#FFFFFFFF"));
+				// if button is clicked, close the custom dialog
+				dialogButton.setOnClickListener(new OnClickListener() {
+					@Override
+					public void onClick(View v) {
+						dialog.dismiss();
+						final Dialog nextDialog = new Dialog(SignUpSignInActivity.this);
+						nextDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+						Drawable d = new ColorDrawable(Color.parseColor("#FFFFFFFF"));
+						d.setAlpha(80);
+						nextDialog.setContentView(R.layout.signupnextcustomdialog);
+						nextDialog.getWindow().setBackgroundDrawable(d);
+						
+						TextView additionalV = (TextView)	 nextDialog.findViewById(R.id.additionalView);
+						additionalV.setTypeface(typeFace);
+						additionalV.setTextColor(Color.parseColor("#FFFFFFFF"));
+						
+						TextView cityV = (TextView)	 nextDialog.findViewById(R.id.cityView);
+						cityV.setTypeface(typeFace);
+						cityV.setTextColor(Color.parseColor("#FFFFFFFF"));
+						
+						TextView countryV = (TextView)	 nextDialog.findViewById(R.id.countryView);
+						countryV.setTypeface(typeFace);
+						countryV.setTextColor(Color.parseColor("#FFFFFFFF"));
+						
+						TextView bioV = (TextView)	 nextDialog.findViewById(R.id.bioView);
+						bioV.setTypeface(typeFace);
+						bioV.setTextColor(Color.parseColor("#FFFFFFFF"));
+						
+						TextView interestV = (TextView)	 nextDialog.findViewById(R.id.interestView);
+						interestV.setTypeface(typeFace);
+						interestV.setTextColor(Color.parseColor("#FFFFFFFF"));
+						
+						Button nextDialogButton = (Button) nextDialog.findViewById(R.id.signUpButton);
+						nextDialogButton.setTypeface(typeFace);
+						nextDialogButton.setTextColor(Color.parseColor("#FFFFFFFF"));
+						nextDialogButton.setOnClickListener(new OnClickListener()
+								{
+
+									@Override
+									public void onClick(View v) {
+										nextDialog.dismiss();
+										
+									}
+							
+								});
+						
+						
+						nextDialog.show();
+						
+					}
+				});  
+				
+			
+	 
+				
+				return false; 
+				}
+				else
+				{
+					return false;
+				}
 			}
 			});
 		
