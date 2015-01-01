29cea363da447d4707a3acca88c1439e7fb9c76b
karivmcmahon <kmc_xo@hotmail.co.uk>
2015-01-01 20:51:14 +0000

    Tidy up

--- RecipesForLife/src/com/example/recipesforlife/models/accountModel.java
+++ RecipesForLife/src/com/example/recipesforlife/models/accountModel.java
@@ -39,6 +39,10 @@ import android.util.Log;
 public class accountModel extends baseDataSource{
 	long id;
 	String str;
+	ContentValues values;
+	ContentValues accountValues;
+	String lastUpdated;
+	
 	public accountModel(Context context) {
 		super(context);
 		// TODO Auto-generated constructor stub
@@ -52,87 +56,8 @@ public class accountModel extends baseDataSource{
             cal.setTime(new Date()); // sets calendar time/date
             cal.add(Calendar.HOUR_OF_DAY, 1); // adds one hour
             Date today = cal.getTime();
-            String lastUpdated = dateToString(today);	 	
-            //User values
-		    ContentValues values = new ContentValues();
-		    values.put("name", accountInfo.get(0)); // inserting a string
-		  //  values.put("secondName", accountInfo.get(1)); // inserting a string
-		    values.put("updateTime", lastUpdated); // inserting a string
-		    values.put("country", accountInfo.get(2)); // inserting a string
-		    values.put("bio", accountInfo.get(3)); // inserting a string
-		    values.put("city", accountInfo.get(4)); // inserting a string
-		    values.put("cookingInterest", accountInfo.get(5)); // inserting a string	    
-		    try
-		    {	
-		    	id = database.insert("Users", null, values);
-			} 
-		    catch (SQLException e) 
-		    {
-		        Log.v("db","Exception 1 " +  e.toString());
-		    } 
-		    //Account values
-		    ContentValues accountValues = new ContentValues();
-		    accountValues.put("id", (int)id);
-		    accountValues.put("email", accountInfo.get(6));
-		    accountValues.put("updateTime", lastUpdated);
-		    accountValues.put("password", accountInfo.get(7));
-		    try
-		    {
-		   	database.insert("Account", null, accountValues);	    
-		    } 
-		    catch (SQLException e) 
-		    {
-		    	Log.v("db","Exception 2 " +  e.toString());
-		    } 
-		    JSONObject student1 = new JSONObject();
-			try {
-				student1.put("name", accountInfo.get(0)); // inserting a string
-				  //  values.put("secondName", accountInfo.get(1)); // inserting a string
-				   
-				    student1.put("country", accountInfo.get(2)); // inserting a string
-				    student1.put("bio", accountInfo.get(3)); // inserting a string
-				    student1.put("city", accountInfo.get(4)); // inserting a string
-				    student1.put("cookingInterest", accountInfo.get(5)); // in
-				    student1.put("id", (int)id);
-				    student1.put("email", accountInfo.get(6));
-				    student1.put("updateTime", lastUpdated);
-				    student1.put("password", accountInfo.get(7));
-				    JSONArray jsonArray = new JSONArray();
-					jsonArray.put(student1);
-					HttpResponse response = null;
-			        HttpClient myClient = new DefaultHttpClient();
-			        HttpPost myConnection = new HttpPost("https://zeno.computing.dundee.ac.uk/2014-projects/karimcmahon/wwwroot/WebForm1.aspx");      	   	
-					try {
-						myConnection.setEntity(new ByteArrayEntity(
-								jsonArray.toString().getBytes("UTF8")));
-						try {
-							response = myClient.execute(myConnection);
-							str = EntityUtils.toString(response.getEntity(), "UTF-8");
-							Log.v("RESPONSE " , "RESPONSE " + str);
-						} catch (ClientProtocolException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						} catch (IOException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						}
-					} catch (UnsupportedEncodingException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-					
-				
-						
-						
-					
-				    Log.v("JSON create ", "JSON create " + jsonArray);
-
-			} catch (JSONException e) {
-			    // TODO Auto-generated catch block
-			    e.printStackTrace();
-			}
-
-			
+            lastUpdated = dateToString(today);	 	
+            insertUserData(accountInfo);
 		    close();
 	} 
 	
@@ -147,20 +72,107 @@ public class accountModel extends baseDataSource{
 		open();
 		Cursor cursor = database.rawQuery("SELECT * FROM Account WHERE email=? AND password=?", new String[] { email, password });
 		if (cursor != null && cursor.getCount() > 0) {
-			Log.v("SIGN IN", "SIGN IN");
 			cursor.close();
 			close();
 			return true;
 		}
 		else
 		{
-			Log.v("SIGN OUT", "SIGN OUT");
 			cursor.close();
 			close();
 			return false;
-		} 
-		
+		} 	
+	}
 	
+	public void createAndSendJSON(List<String> accountInfo)
+	{
+		 JSONObject account = new JSONObject();
+			try 
+			{
+					account.put("name", accountInfo.get(0)); // inserting a string 
+				    account.put("country", accountInfo.get(2)); // inserting a string
+				    account.put("bio", accountInfo.get(3)); // inserting a string
+				    account.put("city", accountInfo.get(4)); // inserting a string
+				    account.put("cookingInterest", accountInfo.get(5)); // in
+				    account.put("id", (int)id);
+				    account.put("email", accountInfo.get(6));
+				    account.put("updateTime", lastUpdated);
+				    account.put("password", accountInfo.get(7));
+				    JSONArray jsonArray = new JSONArray();
+					jsonArray.put(account);
+				
+					HttpResponse response = null;
+			        HttpClient myClient = new DefaultHttpClient();
+			        HttpPost myConnection = new HttpPost("https://zeno.computing.dundee.ac.uk/2014-projects/karimcmahon/wwwroot/WebForm1.aspx");      	   	
+					try 
+					{
+						myConnection.setEntity(new ByteArrayEntity(
+								jsonArray.toString().getBytes("UTF8")));
+						try 
+						{
+							response = myClient.execute(myConnection);
+							str = EntityUtils.toString(response.getEntity(), "UTF-8");
+							
+						} 
+						catch (ClientProtocolException e) 
+						{							
+							e.printStackTrace();
+						} 
+					catch (IOException e) 
+					{
+						e.printStackTrace();
+					}
+				} 
+				catch (UnsupportedEncodingException e) 
+				{						
+					e.printStackTrace();
+				}
+					
+			} 
+			catch (JSONException e) 
+			{
+			    e.printStackTrace();
+			}
+	}
+	
+	public void insertUserData(List<String> accountInfo)
+	{
+		 //User values
+	    values = new ContentValues();
+	    values.put("name", accountInfo.get(0)); 
+	    values.put("updateTime", lastUpdated); 
+	    values.put("country", accountInfo.get(2)); 
+	    values.put("bio", accountInfo.get(3)); 
+	    values.put("city", accountInfo.get(4)); 
+	    values.put("cookingInterest", accountInfo.get(5));     
+	    try
+	    {	
+	    	id = database.insert("Users", null, values);
+	    	insertAccountData(accountInfo, id);
+		} 
+	    catch (SQLException e) 
+	    {
+	        Log.v("db","Exception 1 " +  e.toString());
+	    } 
+	}
+	
+	public void insertAccountData(List<String> accountInfo, long id)
+	{
+		//Account values
+	    accountValues = new ContentValues();
+	    accountValues.put("id", (int)id);
+	    accountValues.put("email", accountInfo.get(6));
+	    accountValues.put("updateTime", lastUpdated);
+	    accountValues.put("password", accountInfo.get(7));
+	    try
+	    {
+	    	database.insert("Account", null, accountValues);
+	    	createAndSendJSON(accountInfo);
+	    } 
+	    catch (SQLException e) 
+	    {
+	    	Log.v("db","Exception 2 " +  e.toString());
+	    } 
 	}
 	
 
--- RecipesForLife/src/com/example/recipesforlife/views/SignUpSignInActivity.java
+++ RecipesForLife/src/com/example/recipesforlife/views/SignUpSignInActivity.java
@@ -1,20 +1,16 @@
 package com.example.recipesforlife.views;
 
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
 
 import com.example.recipesforlife.R;
 import com.example.recipesforlife.models.accountModel;
 import com.example.recipesforlife.models.databaseConnection;
-import com.example.recipesforlife.models.sync;
 
 import android.app.Activity;
 import android.app.Dialog;
 import android.content.Context;
-import android.content.DialogInterface;
-import android.content.DialogInterface.OnDismissListener;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 import android.graphics.Color;
@@ -36,28 +32,27 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class SignUpSignInActivity extends Activity {
+	//Edit text stores
 	EditText emailEdit,nameEdit,passwordEdit,countryEdit, cityEdit, interestEdit, bioEdit;
+	//Strings to store info from edit text box
 	String email,name,password,country,city,interest,bio;
+	//List to store account information
 	List<String> account;
+	//Typeface to change to custom font
 	Typeface typeFace;
+	//Shared prefs to store log in data
 	public static final String MyPREFERENCES = "MyPrefs" ;
 	public static final String emailk = "emailKey"; 
 	public static final String pass = "passwordKey"; 
+	
 	SharedPreferences sharedpreferences;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		 StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-
          StrictMode.setThreadPolicy(policy);
-	//	sync syncs = new sync();
-		/**syncs.getJson();
-		try {
-			syncs.createJsonArray();
-		} catch (UnsupportedEncodingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}**/
+	
+         //View for activity
 		setContentView(R.layout.signupsigninactivity);
 		
 		//Style for activity
@@ -67,29 +62,14 @@ public class SignUpSignInActivity extends Activity {
 		setText(R.id.passwordView, 22);
 		setButtonText(R.id.button1, 22);
 		
+		//Sign in button
 		Button button1 = (Button) findViewById(R.id.button1);
 		button1.setOnClickListener(new OnClickListener() {
-
 			@Override
-			public void onClick(View arg0) {
-				// TODO Auto-generated method stub
-				TextView emailEdit = (TextView) findViewById(R.id.editText1);
-				TextView passwordEdit = (TextView) findViewById(R.id.editText2);
-				String email = emailEdit.getText().toString();
-				String password = passwordEdit.getText().toString();
-				Log.v("SIGN IN ", "Email " + email + " password " + password);
-				Context t = getApplicationContext();
-				accountModel accountmodel = new accountModel(t);
-				boolean access = accountmodel.logIn(email, password);
-				if(access == true)
-				{
-					Log.v("prefs saved","prefs saved");
-					 Editor editor = sharedpreferences.edit();
-				     
-				      editor.putString(emailk, email);
-				      editor.putString(pass, password);
-				      editor.commit();
-				}
+			public void onClick(View arg0) 
+			{	
+				//Check login details
+				checkLogin();
 			}
 			
 		});
@@ -118,9 +98,7 @@ public class SignUpSignInActivity extends Activity {
 				setDialogText(R.id.emailView,dialog,22);
 				setDialogText(R.id.passwordView,dialog,22);
 				setDialogText(R.id.createView,dialog,28);
-	
-				
-				//Show dialogs
+				//Show dialog
 				dialog.show(); 
 				
 				//Next button on dialog
@@ -131,16 +109,8 @@ public class SignUpSignInActivity extends Activity {
 				dialogButton.setOnClickListener(new OnClickListener() {
 					@Override
 					public void onClick(View v) {
-						emailEdit = (EditText) dialog.findViewById(R.id.emailEdit);
-						nameEdit = (EditText) dialog.findViewById(R.id.nameEdit);
-						passwordEdit = (EditText) dialog.findViewById(R.id.passwordEdit);
-						name= nameEdit.getText().toString();
-						password = passwordEdit.getText().toString();
-						email = emailEdit.getText().toString(); 
-						
-						Log.v("NAME ", "NAME " + name);
-						//Dismiss dialog
-						dialog.dismiss();
+						//get the info from the dialog
+						getInitialDialogInfo(dialog);		
 						//Show another dialog
 						final Dialog nextDialog = new Dialog(SignUpSignInActivity.this);
 						nextDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
@@ -154,12 +124,6 @@ public class SignUpSignInActivity extends Activity {
 						setDialogText(R.id.countryView,nextDialog,22);
 						setDialogText(R.id.bioView,nextDialog,22);
 						setDialogText(R.id.interestView,nextDialog,22);
-						
-						
-						
-						
-						
-						
 						//Set button click
 						Button nextDialogButton = (Button) nextDialog.findViewById(R.id.signUpButton);
 						nextDialogButton.setTypeface(typeFace);
@@ -167,59 +131,27 @@ public class SignUpSignInActivity extends Activity {
 						nextDialogButton.setTextSize(22);
 						nextDialogButton.setOnClickListener(new OnClickListener()
 								{
-
 									@Override
-									public void onClick(View v) {
-										cityEdit = (EditText) nextDialog.findViewById(R.id.cityEdit);
-										countryEdit = (EditText) nextDialog.findViewById(R.id.countryEdit);
-										bioEdit = (EditText) nextDialog.findViewById(R.id.bioEditText);
-										interestEdit = (EditText) nextDialog.findViewById(R.id.interestEditText);
-										city = cityEdit.getText().toString();
-										country = countryEdit.getText().toString();
-										bio = bioEdit.getText().toString();
-										interest = interestEdit.getText().toString();
-										
-										account.add(name);
-										account.add(name);
-										account.add(country);
-										account.add(bio);
-										account.add(city);
-										account.add(interest);
-										account.add(email);
-										account.add(password);
-										
-										//Insert to db
-										Context t = getApplicationContext();
-										accountModel accountmodel = new accountModel(t);
-										accountmodel.insertAccount(account);
-										nextDialog.dismiss();
-										
+									public void onClick(View v) 
+									{
+										getSecondDialogInfo(nextDialog);
 									}
 							
-								});
-						
-						
-						nextDialog.show();
-						
+								});					
+						nextDialog.show();						
 					}
 				});  
-				
-			
-	 
-				
-				return false; 
-				}
-				else
-				{
-					return false;
-				}
+							
+			return false; 
 			}
-			});
-		
-		
-		
+			else
+			{
+				return false;
+			}
+		}
+	});
 		buildDatabase();
-	}
+}
 	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -291,5 +223,68 @@ public class SignUpSignInActivity extends Activity {
 			view.setTextColor(Color.parseColor("#FFFFFFFF"));
 		}
 		
+		public void checkLogin()
+		{
+			//Get text from edit textbox
+			EditText emailEdit = (EditText) findViewById(R.id.editText1);
+			EditText passwordEdit = (EditText) findViewById(R.id.editText2);
+			String email = emailEdit.getText().toString();
+			String password = passwordEdit.getText().toString();
+			//Check if these details are correct
+			Context t = getApplicationContext();
+			accountModel accountmodel = new accountModel(t);
+			boolean access = accountmodel.logIn(email, password);
+			//If allowed access save pref
+			if(access == true)
+			{
+			  Log.v("prefs saved","prefs saved");
+			  Editor editor = sharedpreferences.edit();
+		      editor.putString(emailk, email);
+		      editor.putString(pass, password);
+		      editor.commit();
+			}
+		}
+		
+		public void getInitialDialogInfo(Dialog dialog)
+		{
+			emailEdit = (EditText) dialog.findViewById(R.id.emailEdit);
+			nameEdit = (EditText) dialog.findViewById(R.id.nameEdit);
+			passwordEdit = (EditText) dialog.findViewById(R.id.passwordEdit);
+			name= nameEdit.getText().toString();
+			password = passwordEdit.getText().toString();
+			email = emailEdit.getText().toString(); 	
+			//Dismiss dialog
+			dialog.dismiss();
+			
+		}
+		
+		public void getSecondDialogInfo(Dialog nextDialog)
+		{
+			cityEdit = (EditText) nextDialog.findViewById(R.id.cityEdit);
+			countryEdit = (EditText) nextDialog.findViewById(R.id.countryEdit);
+			bioEdit = (EditText) nextDialog.findViewById(R.id.bioEditText);
+			interestEdit = (EditText) nextDialog.findViewById(R.id.interestEditText);
+			city = cityEdit.getText().toString();
+			country = countryEdit.getText().toString();
+			bio = bioEdit.getText().toString();
+			interest = interestEdit.getText().toString();
+			
+			account.add(name);
+			account.add(name);
+			account.add(country);
+			account.add(bio);
+			account.add(city);
+			account.add(interest);
+			account.add(email);
+			account.add(password);
+			
+			//Insert to db
+			Context t = getApplicationContext();
+			accountModel accountmodel = new accountModel(t);
+			accountmodel.insertAccount(account);
+			nextDialog.dismiss();
+		}
+	}
+		
 
-}
+
