a9b5702782849745c393163a515e956f576392d3
karivmcmahon <kmc_xo@hotmail.co.uk>
2015-01-23 19:46:57 +0000

    Updated website code

---
+++ WebApplication1/WebApplication1/WebForm2.aspx.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.Script.Serialization;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace WebApplication1
+{
+    public partial class WebForm2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Response.Clear();
+            string jsonInput = new System.IO.StreamReader(Context.Request.InputStream, System.Text.Encoding.UTF8).ReadToEnd();
+            if (jsonInput != null)
+            {
+
+                //Serializing a json
+                JavaScriptSerializer js = new JavaScriptSerializer();
+                /**  Employee emp = new Employee();
+                  emp.Name = "a";
+                  emp.age = 12;
+                  string json = js.Serialize(emp); **/
+
+                var p2 = js.Deserialize<List<Date>>(jsonInput);
+                string lastUpdated = p2[0].updateTime;
+                SqlConnection con = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["SQLDbConnection"].ConnectionString);
+                SqlConnection con2 = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["SQLDbConnection"].ConnectionString);
+                SqlCommand select = new SqlCommand(" SELECT * FROM Account WHERE updateTime > @lastUpdated", con);
+                SqlCommand select2 = new SqlCommand(" SELECT * FROM Users WHERE updateTime > @lastUpdated", con2);
+                select.Parameters.AddWithValue("@lastUpdated", lastUpdated);
+                select2.Parameters.AddWithValue("@lastUpdated", lastUpdated);
+                con.Open();
+                con2.Open();
+                var reader = select.ExecuteReader();
+                
+                var list = new List<Acc>();
+                var list2 = new List<User>();
+                
+                int count = reader.FieldCount;
+                var reader2 = select2.ExecuteReader();
+                while (reader.Read())
+                {
+                    Acc account = new Acc();
+                    account.email = (string)reader["email"];
+                    account.password = (string)reader["password"];
+                    
+                    while (reader2.Read())
+                    {
+
+                        User user = new User();
+                        user.name = (string)reader2["name"];
+                        user.bio = (string)reader2["bio"];
+                        user.city = (string)reader2["city"];
+                        user.country = (string)reader2["country"];
+                        user.cookingInterest = (string)reader2["cookingInterest"];
+                        list2.Add(user);
+                        
+                       
+                    }
+                    list.Add(account);
+                 
+                }
+                var fulllist = new List<Account>();
+                for (int i = 0; i < list.Count; i++ )
+                {
+                    Account account = new Account();
+                    account.email = list[i].email;
+                    account.password = list[i].password;
+                    account.city = list2[i].city;
+                    account.country = list2[i].country;
+                    account.bio = list2[i].bio;
+                    account.cookingInterest = list2[i].cookingInterest;
+                    account.name = list2[i].name;
+                    fulllist.Add(account);
+
+                }
+                    con2.Close();
+                con.Close();
+               
+                string json = js.Serialize(fulllist);
+				list.Clear();
+                Response.Write(json);
+			    json = "";
+            }
+        }
+    }
+
+    public class Date
+    {
+        public string updateTime { get; set; }
+    }
+    public class Account
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string updateTime { get; set; }
+        public string country { get; set; }
+        public string city { get; set; }
+        public string bio { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public string cookingInterest { get; set; }
+    }
+
+    public class Acc
+    {
+        public string email { get; set; }
+        public string password { get; set; }
+
+
+    }
+
+    public class User
+    {
+        public string name { get; set; }
+        public string updateTime { get; set; }
+        public string country { get; set; }
+        public string city { get; set; }
+        public string bio { get; set; }
+        public string cookingInterest { get; set; }
+    }
+
+}
+
---
+++ WebApplication1/WebApplication1/WebForm3.aspx.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.Script.Serialization;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace WebApplication1
+{
+    public partial class WebForm3 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+             string jsonInput = new System.IO.StreamReader(Context.Request.InputStream, System.Text.Encoding.UTF8).ReadToEnd();
+             if (jsonInput != null)
+             {
+                 JavaScriptSerializer js = new JavaScriptSerializer();
+                 var recipe = js.Deserialize<List<Recipe>>(jsonInput);
+                 for(int i = 0; i < recipe.Count(); i++)
+                 {
+                     SqlConnection connn = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["SQLDbConnection"].ConnectionString);
+                     SqlCommand insert = new SqlCommand(" INSERT INTO Recipe(name, description, updateTime, serves, prepTime, cookingTime, addedBy) OUTPUT INSERTED.id VALUES (@name, @description, @updateTime, @serves, @prepTime, @cookingTime, @addedBy)", connn);
+                     insert.Parameters.AddWithValue("@name", recipe[i].name);
+                     insert.Parameters.AddWithValue("@description", recipe[i].description);
+                     insert.Parameters.AddWithValue("@serves", recipe[i].serves);
+                     insert.Parameters.AddWithValue("@prepTime", recipe[i].prepTime);
+                     insert.Parameters.AddWithValue("@cookingTime", recipe[i].cookingTime);
+                     insert.Parameters.AddWithValue("@addedBy", recipe[i].addedBy);
+                     insert.Parameters.AddWithValue("@updateTime", recipe[i].updateTime);
+
+                    connn.Open();
+                     try
+                     {
+                         
+                         Int32 recipeId = (Int32)insert.ExecuteScalar();
+                         Response.Write(recipeId);
+                  
+                        
+                         for (int y = 0; y < recipe[i].Preperation[0].prep.Count(); y++)
+                         {
+                                SqlCommand insert2 = new SqlCommand(" INSERT INTO Preperation(instruction, instructionNum, updateTime) OUTPUT INSERTED.id VALUES (@prep, @prepNums, @updateTime)", connn);
+                                 insert2.Parameters.AddWithValue("@prep", recipe[i].Preperation[0].prep[y]);
+                                 insert2.Parameters.AddWithValue("@prepNums", recipe[i].Preperation[1].prepNums[y]);
+                                 insert2.Parameters.AddWithValue("@updateTime", recipe[i].updateTime);
+                                 try
+                                 {
+
+                                     Int32 prepId = (Int32)insert2.ExecuteScalar();
+                                     Response.Write(prepId);
+                                     SqlCommand insert3 = new SqlCommand(" INSERT INTO PrepRecipe(recipeId, Preperationid, updateTime)  VALUES (@recipeId, @Preperationid, @updateTime)", connn);
+                                     insert3.Parameters.AddWithValue("@recipeId", recipeId);
+                                     insert3.Parameters.AddWithValue("@Preperationid", prepId);
+                                     insert3.Parameters.AddWithValue("@updateTime", recipe[i].updateTime);
+                                     try
+                                     {
+
+                                         insert3.ExecuteReader();
+               
+                                     }
+                                     catch (Exception ex)
+                                     {
+
+                                         Response.Write("Error ");
+                                         Response.Write(ex);
+                                     }
+                                 }
+                                 catch (Exception ex)
+                                 {
+
+                                     Response.Write("Error ");
+                                     Response.Write(ex);
+                                 }
+                                 
+                             
+                             //  Response.Write(recipe[i].Preperation[1].prepNums[z] + "\n");
+                             
+                             //  Response.Write(recipe[i].Preperation[0].prep[y] + "\n");
+                         }
+
+                        
+                         
+                        
+                     }
+                     catch (Exception ex)
+                     {
+
+                         Response.Write("Error ");
+                         Response.Write(ex);
+                     }
+                     connn.Close();
+
+                       
+                     
+                         
+
+                        for(int a = 0; a < recipe[i].Ingredient[0].Ingredients.Count(); a++)
+                        {
+                          //  Response.Write(recipe[i].Ingredient[0].Ingredients[a]);
+                        }
+
+                        for(int b = 0; b < recipe[i].Ingredient[1].Value.Count(); b++)
+                        {
+                          //  Response.Write(recipe[i].Ingredient[1].Value[b]);
+                        }
+
+                        for(int c = 0; c < recipe[i].Ingredient[2].Amount.Count(); c++)
+                        {
+                         //   Response.Write(recipe[i].Ingredient[2].Amount[c]);
+                        }
+
+                        for(int d = 0; d < recipe[i].Ingredient[3].Notes.Count(); d++)
+                         {
+                            // Response.Write(recipe[i].Ingredient[3].Notes[d]);
+                         }
+
+                     
+                    // Response.Write(recipe[i].Preperation.prepNums[0]);
+                 }
+             }
+        }
+    }
+}
+
+public class Recipe
+{
+    public string name { get; set; }
+    public string description { get; set; }
+    public string serves { get; set; }
+    public string prepTime { get; set; }
+    public string cookingTime { get; set; }
+    public string addedBy { get; set; }
+    public string updateTime { get; set;  }
+    public List<Preperation> Preperation { get; set; }
+    public List<Ingredient> Ingredient { get; set; }
+}
+
+public class Preperation
+{
+    public  List<String> prep { get; set; }
+    public List<String> prepNums { get; set; }
+  //  public int prepNums { get; set; }
+}
+
+public class Ingredient
+{
+    public List<String> Ingredients { get; set; }
+    public List<String> Value { get; set; }
+    public List<String> Amount { get; set; }
+    public List<String> Notes { get; set; }
+}
