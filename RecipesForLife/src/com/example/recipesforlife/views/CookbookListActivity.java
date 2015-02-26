f0c34f8c4604b9ea6dc6c313da6912ac20933f94
karivmcmahon <kmc_xo@hotmail.co.uk>
2015-02-26 20:18:05 +0000

    Action bar

--- RecipesForLife/src/com/example/recipesforlife/views/CookbookListActivity.java
+++ RecipesForLife/src/com/example/recipesforlife/views/CookbookListActivity.java
@@ -9,6 +9,8 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.graphics.Shader;
 import android.graphics.Typeface;
@@ -16,10 +18,15 @@ import android.graphics.drawable.BitmapDrawable;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.StrictMode;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarActivity;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
@@ -27,6 +34,19 @@ import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
+
+
+
+
+
+
+
+
+
+
+
+
+import android.widget.Toast;
 
 import com.example.recipesforlife.R;
 import com.example.recipesforlife.controllers.cookbookBean;
@@ -38,13 +58,20 @@ import com.example.recipesforlife.models.util;
  * @author Kari
  *
  */
-public class CookbookListActivity extends Activity {
+public class CookbookListActivity extends ActionBarActivity {
 
 	ListView listView;
 	public static final String MyPREFERENCES = "MyPrefs";
 	ArrayList<cookbookBean> cookbookList;
+	public static final String pass = "passwordKey"; 
 	String type = "";
 	util utils;
+	
+	 private String[] mPlanetTitles;
+	    private DrawerLayout mDrawerLayout;
+	    private ListView mDrawerList;
+	    private CharSequence mTitle;
+	    private ActionBarDrawerToggle mDrawerToggle;
 
 	public static final String emailk = "emailKey";
 	@SuppressLint("NewApi")
@@ -55,10 +82,47 @@ public class CookbookListActivity extends Activity {
 		StrictMode.setThreadPolicy(policy);
 		setContentView(R.layout.listview);
 		
-		getActionBar().setTitle("My Cookbooks");
-		centerActionBarTitle();
+		getSupportActionBar().setTitle("My Cookbooks");
+		//centerActionBarTitle();
 
 	
+		 
+        mPlanetTitles = new String[]{"Home", "Settings", "Profile", "Log Off"};
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+ 
+        // Set the adapter for the list view
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this,
+                R.layout.drawer_list_item, mPlanetTitles));
+       
+ 
+        mDrawerToggle = new ActionBarDrawerToggle(
+                this,                  /* host Activity */
+                mDrawerLayout,         /* DrawerLayout object */
+                R.drawable.menu,  /* nav drawer icon to replace 'Up' caret */
+                R.string.drawer_open,  /* "open drawer" description */
+                R.string.drawer_close  /* "close drawer" description */
+                ) {
+ 
+            /** Called when a drawer has settled in a completely closed state. */
+            public void onDrawerClosed(View view) {
+                getSupportActionBar().setTitle("My Cookbooks");
+            }
+ 
+            /** Called when a drawer has settled in a completely open state. */
+            public void onDrawerOpened(View drawerView) {
+                getSupportActionBar().setTitle("My Cookbooks");
+                mDrawerList.bringToFront();
+                mDrawerLayout.requestLayout();
+            }
+        };
+ 
+        // Set the drawer toggle as the DrawerListener
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+ 
+      getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+     //  getSupportActionBar().setHomeButtonEnabled(true);
+       mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
 		
 		utils = new util(getApplicationContext(), this);
 		
@@ -94,60 +158,93 @@ public class CookbookListActivity extends Activity {
 		CustomCookbookListAdapter adapter = new CustomCookbookListAdapter(this, values, getApplicationContext(), ids);
 		listView.setAdapter(adapter); 
 		
-		//When item clicked move to next activity
-		/**listView.setOnItemClickListener(new OnItemClickListener() {
-			@Override
-			public void onItemClick(AdapterView<?> arg0, View arg1, int position,
-					long arg3) {
-				Intent i = new Intent(CookbookListActivity.this, RecipeListViewActivity.class);
-				
-
-				// ListView Clicked item value - gets matching uniqueid
-				String  itemValue    = (String) listView.getItemAtPosition(position);
-				String uniqueid = "";
-				for(int a = 0; a < cookbookList.size(); a++)
-				{
-					if(itemValue.equals(cookbookList.get(a).getName()))
-					{
-						uniqueid = cookbookList.get(a).getUniqueid();
-					}
-				}
-				i.putExtra("uniqueid", uniqueid);
-				i.putExtra("type", type);
-				startActivity(i);
-
-			}
-		}); **/
+		
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+
 
 	@Override
 	protected void onResume() {
 		super.onResume();
 	}
 	
-	
+	 @Override
+	    protected void onPostCreate(Bundle savedInstanceState) {
+	        super.onPostCreate(savedInstanceState);
+	        // Sync the toggle state after onRestoreInstanceState has occurred.
+	        mDrawerToggle.syncState();
+	    }
+	 
+	  @Override
+	    public void onConfigurationChanged(Configuration newConfig) {
+	        super.onConfigurationChanged(newConfig);
+	        mDrawerToggle.onConfigurationChanged(newConfig);
+	    }
+	 
+	    @Override
+	    public boolean onOptionsItemSelected(MenuItem item) {
+	    	Log.v("Click c", "Click c");
+	        // Pass the event to ActionBarDrawerToggle, if it returns
+	        // true, then it has handled the app icon touch event
+	        if (mDrawerToggle.onOptionsItemSelected(item)) {
+	            return true;
+	        }
+	        
+	        // Handle your other action bar items...
+	 
+	        return super.onOptionsItemSelected(item);
+	    }
+	 
+	    /**
+	     * Swaps fragments in the main content view
+	     */
+	  
+	 
+	    @Override
+	    public void setTitle(CharSequence title) {
+	        mTitle = title;
+	        getSupportActionBar().setTitle(mTitle);
+	    }
+	    
+	    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+	        @Override
+	        public void onItemClick(AdapterView parent, View view, int position, long id) {
+	        	if(position == 0)
+	        	{
+	        		Intent i = new Intent(CookbookListActivity.this, CookbookListActivity.class);
+	        		startActivity(i);
+	        	}
+	        	else if(position == 3)
+	        	{
+	        		  SharedPreferences sharedpreferences = getSharedPreferences
+						      (SignUpSignInActivity.MyPREFERENCES, Context.MODE_PRIVATE);
+						      Editor editor = sharedpreferences.edit();
+						     editor.remove(emailk);
+						     editor.remove(pass);
+						     // editor.clear();
+						      editor.commit();
+						      Intent i = new Intent(CookbookListActivity.this, SignUpSignInActivity.class);
+						     startActivity(i);
+	        	}
+	            Toast.makeText(CookbookListActivity.this, ((TextView)view).getText(), Toast.LENGTH_LONG).show();
+	            mDrawerLayout.closeDrawer(mDrawerList);
+	 
+	        }
+	    }
+	 
+	   
 
 
 	private void centerActionBarTitle()
 	    {
-
+			
 	        int titleId = 0;
-	        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB)
-	        {
+	        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
 	            titleId = getResources().getIdentifier("action_bar_title", "id", "android");
-	        }
-	        else
-	        {
-	            // This is the id is from your app's generated R class when ActionBarActivity is used 
-	            // for SupportActionBar
-	         //   titleId = R.id.ac
+	            Log.v("titleid", "titleid" + titleId);
+	        } else {
+	        	titleId = R.id.action_bar_title;
+	            
 	        }
 
 	        // Final check for non-zero invalid id
@@ -158,16 +255,20 @@ public class CookbookListActivity extends Activity {
 	            DisplayMetrics metrics = getResources().getDisplayMetrics();
 
 	            // Fetch layout parameters of titleTextView (LinearLayout.LayoutParams : Info from HierarchyViewer)
-	            LinearLayout.LayoutParams txvPars = (LayoutParams) titleTextView.getLayoutParams();
-	            txvPars.gravity = Gravity.CENTER_HORIZONTAL;
-	            txvPars.width = metrics.widthPixels;
-	            titleTextView.setLayoutParams(txvPars);
-	            Typeface typeFace=Typeface.createFromAsset(getApplicationContext().getAssets(),"fonts/elsie.ttf");
-	            titleTextView.setTypeface(typeFace);
+	          //  LinearLayout.LayoutParams txvPars = (LayoutParams) titleTextView.getLayoutParams();
+	          //  txvPars.gravity = Gravity.CENTER_HORIZONTAL;
+	           // txvPars.width = metrics.widthPixels;
+	          //  titleTextView.setLayoutParams(txvPars);
+	          //  Typeface typeFace=Typeface.createFromAsset(getApplicationContext().getAssets(),"fonts/elsie.ttf");
+	         //   titleTextView.setTypeface(typeFace);
 	            titleTextView.setTextSize(28);
-	            titleTextView.setText("My Cookbooks");
-	            titleTextView.setGravity(Gravity.CENTER);
+	           // titleTextView.setText("My Cookbooks");
+	          //  titleTextView.setGravity(Gravity.CENTER);
 	        }
 	    }
 
-}
+
+
+   }
+
+
